94ba3b02156e8def3fde2a7624aa755464b9cc53
Neemias Cormino <ncormino@gmail.com>
2025-10-09 17:07:29 -0300

    fix: viewer app in expo go

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="nativewind/types" />
+
+/// <reference types="expo/types" />
+
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
